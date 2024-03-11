56d888957a3324ccff3e663a691cc7f9d4d33913
Logan-D00255031 <145552065+Logan-D00255031@users.noreply.github.com>
2024-03-11 17:47:31 +0000

    Implemented insertCar function from my code into Ida's CarDaoInterface and MySqlCarDao

--- src/main/java/org/example/Ida/DAOs/CarDaoInterface.java
+++ src/main/java/org/example/Ida/DAOs/CarDaoInterface.java
@@ -1,12 +1,14 @@
 package org.example.Ida.DAOs;
+import java.sql.SQLException;
 import java.util.List;
 import org.example.Ida.Exceptions.DaoException;
 import org.example.Ida.DTOs.CarClass;
+import org.example.Logan.Car;
 
 import java.util.List;
 
 public interface CarDaoInterface {
     public List<CarClass> findAllCars() throws DaoException;
 
-
+    public int insertCar(CarClass car) throws SQLException;
 }
--- src/main/java/org/example/Ida/DAOs/MySqlCarDao.java
+++ src/main/java/org/example/Ida/DAOs/MySqlCarDao.java
@@ -1,6 +1,8 @@
 package org.example.Ida.DAOs;
 import org.example.Ida.DTOs.CarClass;
 import org.example.Ida.Exceptions.DaoException;
+import org.example.Logan.Car;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -66,5 +68,70 @@ public class MySqlCarDao extends MySqlDao implements CarDaoInterface{
             }
         }
         return carsList;     // may be empty
+    }
+
+    // Logan Rushe
+    /**
+     * Insert a given Car into the car table
+     * @param car The car object to be inserted
+     * @return Code 1 if successful, or 0 if already contained within the table
+     * @throws SQLException
+     */
+    public int insertCar(CarClass car) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        PreparedStatement checkStatement = null;
+        ResultSet resultSet = null;
+        int code;
+
+        try {
+            connection = this.getConnection();
+
+            // Prepare insert statement
+            String query = "INSERT INTO car VALUES (null, ?, ?, ?, ?, ?)";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, car.getModel());
+            preparedStatement.setString(2, car.getBrand());
+            preparedStatement.setString(3, car.getColour());
+            preparedStatement.setInt(4, car.getProduction_year());
+            preparedStatement.setInt(5, car.getPrice());
+
+            // Prepare statement to check if the car is in the table already
+            String checkQuery = "SELECT * FROM car WHERE model = ? AND brand = ?";
+            checkStatement = connection.prepareStatement(checkQuery);
+            checkStatement.setString(1, car.getModel());
+            checkStatement.setString(2, car.getBrand());
+            resultSet = checkStatement.executeQuery();
+
+            // If the car is found in the table
+            if(resultSet.next())
+                code = 0;
+            else
+                code = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("insertCar() " + e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null)
+                {
+                    resultSet.close();
+                }
+                if (preparedStatement != null)
+                {
+                    preparedStatement.close();
+                }
+                if (checkStatement != null)
+                {
+                    checkStatement.close();
+                }
+                if (connection != null)
+                {
+                    freeConnection(connection);
+                }
+            } catch (SQLException e) {
+                throw new SQLException("insertCar() " + e.getMessage());
+            }
+        }
+        return code;
     }
 }
