f41338108e0ff0be7fe4c9c1c5c7d06967f84558
Logan-D00255031 <145552065+Logan-D00255031@users.noreply.github.com>
2024-03-11 17:21:05 +0000

    Added Tests for insertCar function

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.Test;
+import java.sql.SQLException;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    MySqlCarDao ICarDao = new MySqlCarDao();
+
+    @Test
+    public void insertTest1() throws SQLException {
+        assertEquals(1, ICarDao.insertCar(new Car(1, "model", "brand", "colour", 2000, 10000)));
+    }
+
+    @Test
+    public void insertTest2() throws SQLException {
+        assertEquals(0, ICarDao.insertCar(new Car(1, "model", "brand", "red", 2001, 11000)));
+    }
+}
