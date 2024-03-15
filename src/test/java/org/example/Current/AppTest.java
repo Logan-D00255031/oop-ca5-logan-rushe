67b60e4f0d5dfef304001f06d60dbc82a265d6eb
Logan-D00255031 <145552065+Logan-D00255031@users.noreply.github.com>
2024-03-15 15:10:52 +0000

    Added Insert Test for Current App

---
+++ src/test/java/org/example/Current/AppTest.java
@@ -0,0 +1,21 @@
+package org.example.Current;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    MySqlCarDao ICarDao = new MySqlCarDao();
+
+    @Test
+    public void insertTest1() throws SQLException {
+        assertEquals(1, ICarDao.insertCar(new Car(1, "model", "brand", "colour", 2000, 10000)));
+    }
+
+    @Test
+    public void insertTest2() throws SQLException {
+        assertEquals(0, ICarDao.insertCar(new Car(1, "model", "brand", "red", 2001, 11000)));
+    }
+}
